7ae8f800a4cce113e81c71f3a2c531fab99356b4
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-07-22 19:01:06 +0900

    Revert "wip"
    This reverts commit 6a9d3d37ea5c606d2e7d2ffefc6150c6c9806388.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,20 +6,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="referrer" content="always">
+        <link rel="canonical" href="{{ $page->getUrl() }}">
 
-        <title>Admin</title>
+        <meta name="description" content="{{ $page->description }}">
 
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <title>{{ $page->title }}</title>
 
-        <script src="{{ mix('js/app.js') }}"></script>
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+
+        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </head>
 
     <body>
         <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200 font-roboto">
-            @include('layouts.sidebar')
+            @include('_layouts.sidebar')
 
             <div class="flex-1 flex flex-col overflow-hidden">
-                @include('layouts.header')
+                @include('_layouts.header')
 
                 <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
                     <div class="container mx-auto px-6 py-8">

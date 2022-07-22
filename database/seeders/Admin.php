b9fd4a78a28228d79fdcb3dc09335d15a4fca5e4
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-07-22 19:03:48 +0900

    Revert "Revert "wip""
    This reverts commit 7ae8f800a4cce113e81c71f3a2c531fab99356b4.

--- database/seeders/Admin.php
+++ database/seeders/Admin.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class Admin extends Seeder
 {
@@ -17,9 +18,9 @@ class Admin extends Seeder
     {
         User::create([
             'name' => 'admin',
-            'email' => 'admin@gmail.com',
+            'email' => 'admin@example.com',
             'email_verified_at' => now(),
-            'password' => bcrypt('password'),
+            'password' => Hash::make('password'),
             'is_admin' => 1,
         ]);
     }

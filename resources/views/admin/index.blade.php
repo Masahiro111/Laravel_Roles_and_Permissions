bd95e6d8a2da597a683770fd75525841dd6d3804
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-07-22 19:02:33 +0900

    Revert "finished 5"
    This reverts commit 3935355210f50b768a3cd05f7e96db907ca4722d.

--- resources/views/admin/index.blade.php
+++
@@ -1,7 +0,0 @@
-<x-admin-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('admin') }}
-        </h2>
-    </x-slot>
-</x-admin-layout>
